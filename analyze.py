79a5f08997a72abfb7158f55c50700ba32f596e4
Nick Amin <amin.nj@gmail.com>
2019-01-12 05:02:01 -0800

    add scripts

---
+++ analyze.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+from reduction import num_metacategories
+
+if __name__ == "__main__":
+
+    df = pd.read_pickle("combined_data.pkl")
+
+    # print df.head()
+
+    print df.text
+
+    # Unpack column by column into an num_review-by-num_metacategories matrix again
+    target_vecs = np.vstack([
+        df["cat_{}".format(i)] for i in range(num_metacategories)
+        ])
+    print target_vecs
+
